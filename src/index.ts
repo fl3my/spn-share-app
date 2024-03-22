82918ff2a02d030c7fe287fd200f1950fe34dd3b
fl3my <rossflem@icloud.com>
2024-03-22 16:45:00 +0000

    Add express package

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello world");
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+// configure environment variables
+dotenv.config();
+
+// Create an express application
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Use morgan to log HTTP requests
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start the express server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
