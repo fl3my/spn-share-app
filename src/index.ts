223e8542cf1020120929db939d65468e716662ad
fl3my <rossflem@icloud.com>
2024-03-22 16:48:47 +0000

    Add handlebars as the view engine

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import { engine } from "express-handlebars";
 
 // configure environment variables
 dotenv.config();
@@ -12,8 +13,20 @@ const port = process.env.PORT || 3000;
 // Use morgan to log HTTP requests
 app.use(morgan("dev"));
 
+// Configure the handlebars engine
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+
+// Set the view engine and views directory
+app.set("view engine", ".hbs");
+app.set("views", "src/views");
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
 });
 
 // Start the express server
